efcfad79490131b863f9c19306146310428841f3
penny <115842020+peachypenny@users.noreply.github.com>
2023-11-09 10:26:55 -0500

    Add files via upload
    This is the file to put your rough code, outlines, pseudo code! :)

---
+++ first_draft.py
@@ -0,0 +1,7 @@
+"""
+Put your first batch of code into this file 
+to submit for check-in.
+If you need a refresher on what you are 
+responsible for, check the outline we created 
+in the shared folder.
+"""
