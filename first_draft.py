e25e75d9d9ad3ee415a61919491816cbf4a28810
anishabahl03 <101065093+anishabahl03@users.noreply.github.com>
2023-11-09 23:45:01 -0500

    Update first_draft.py

--- first_draft.py
+++ first_draft.py
@@ -1,7 +1,17 @@
-"""
-Put your first batch of code into this file 
-to submit for check-in.
-If you need a refresher on what you are 
-responsible for, check the outline we created 
-in the shared folder.
-"""
+def start_game():
+    name = input("Enter your name: ")
+    
+    game_introduction = f"Hi {name}, welcome to wordle. Here's how you play the game: "
+    
+    game_rule = f"You have 6 tries to guess a 5 letter word"
+    
+    word_gray = f"If the letter in the guessed word is gray, that means that the letter is not in the word"
+    
+    word_yellow = f"If the letter in the guessed word is yellow, that means that the letter is in the word but not in the correct position"
+    
+    word_green = f"Last but not least, if the letter in the guessed word is green, that means that the letter is in the correct position"
+    
+    return name + "/n" + game_introduction + "/n" + word_gray + "/n" + word_yellow + "/n" + word_green
+
+    
+    
