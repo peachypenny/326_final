f297554c72311cf548da9f4c27f0f328ad3354a4
Penelope Newcomb <pnewcomb03@gmail.com>
2023-12-05 18:58:24 -0500

    update w/ penelope's changes

--- presentation.py
+++ presentation.py
@@ -1,1 +1,12 @@
 #presentation file
+
+"""
+Future Features:
+- save game state
+class Player():
+    # Allows us to track score. To be called on in the game_scores method.
+    def __init__(self, name):
+         self.name = ""
+         self.score = 0
+         self.total_score = 0
+"""
