1cd88d3ab5327596959def0074b75774d222a0fb
aliyahviray <aliyahxviray@gmail.com>
2023-12-05 22:14:01 -0500

    fixed indents

--- presentation.py
+++ presentation.py
@@ -51,24 +51,24 @@ class WordleGame:
 
 
 def main():
-   name = get_user_info()
+    name = get_user_info()
   
-   parser = argparse.ArgumentParser(description='Wordle Game')
-   parser.add_argument('filepath', type=str, help='Path to the words file')
-   args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Wordle Game')
+    parser.add_argument('filepath', type=str, help='Path to the words file')
+    args = parser.parse_args()
 
 
-   wordle_game = WordleGame(args.filepath)
+    wordle_game = WordleGame(args.filepath)
 
 
-   play_again = 'yes'
-   while play_again.lower() == 'yes':
-       wordle_game.play_round()
-       play_again = input("Do you want to play again? (yes/no): ")
+    play_again = 'yes'
+    while play_again.lower() == 'yes':
+        wordle_game.play_round()
+        play_again = input("Do you want to play again? (yes/no): ")
 
 
-   print("Thanks for playing. Goodbye!")
+    print("Thanks for playing. Goodbye!")
 
 
 if __name__ == "__main__":
-   main()
+    main()
