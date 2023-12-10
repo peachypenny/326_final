7c332967a5af0859cc2b7f5c0092b7c879fade64
anishabahl03 <101065093+anishabahl03@users.noreply.github.com>
2023-12-10 13:23:40 -0500

    Update first_draft.py

--- first_draft.py
+++ first_draft.py
@@ -14,14 +14,15 @@ class Player():
          self.total_score = 0
 
 #Anisha's method 
-    def start_game():
+    def get_user_info():
+
         name = input("Enter your name: ")
-        game_introduction = f"Hi {name}, welcome to wordle. Here's how you play the game: "
-        game_rule = f"You have 6 tries to guess a 5 letter word"
-        word_gray = f"If the letter in the guessed word is gray, that means that the letter is not in the word"
-        word_yellow = f"If the letter in the guessed word is yellow, that means that the letter is in the word but not in the correct position"
-        word_green = f"Last but not least, if the letter in the guessed word is green, that means that the letter is in the correct position"  
-        return name + "/n" + game_introduction + "/n" + word_gray + "/n" + word_yellow + "/n" + word_green
+        print(f"Hi {name}, welcome to Wordle. Here's how you play the game:")
+        print("You have 6 tries to guess a 5-letter word.")
+        print("If the letter in the guessed word is gray, that means that the letter is not in the word.")
+        print("If the letter in the guessed word is yellow, that means that the letter is in the word but not in the correct position.")
+        print("If the letter in the guessed word is green, that means that the letter is in the correct position.")
+        return name
 
 #Aliyah's methods
 def __init__(self, filepath):
